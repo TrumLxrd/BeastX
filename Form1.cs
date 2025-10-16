887613011650120452be84942a741422c94338a3
Adham Shouka <73558403+TrumLxrd@users.noreply.github.com>
2025-10-16 13:16:23 +0300

    Deprecated.

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3rd_try
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flatButton1_Click(object sender, EventArgs e)
+        {
+            if (flatTextBox1.Text == "lol")
+            {
+                MessageBox.Show("Thanks for using this exploit");
+                this.Hide();
+                BeastX main = new BeastX();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+            public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void FormMoveable_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+    }
+
---
+++ Form6.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace _3rd_try
+{
+    partial class BeastX
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BeastX));
+            this.formSkin1 = new FlatUI.FormSkin();
+            this.flatMini1 = new FlatUI.FlatMini();
+            this.flatClose1 = new FlatUI.FlatClose();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flatButton3 = new FlatUI.FlatButton();
+            this.flatButton6 = new FlatUI.FlatButton();
+            this.flatButton7 = new FlatUI.FlatButton();
+            this.flatButton5 = new FlatUI.FlatButton();
+            this.flatButton4 = new FlatUI.FlatButton();
+            this.flatButton2 = new FlatUI.FlatButton();
+            this.flatButton1 = new FlatUI.FlatButton();
+            this.flatButton8 = new FlatUI.FlatButton();
+            this.formSkin1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+            this.formSkin1.Controls.Add(this.flatMini1);
+            this.formSkin1.Controls.Add(this.flatClose1);
+            this.formSkin1.Controls.Add(this.fastColoredTextBox1);
+            this.formSkin1.Controls.Add(this.panel1);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(612, 308);
+            this.formSkin1.TabIndex = 0;
+            this.formSkin1.Text = "BeastX";
+            // 
+            // flatMini1
+            // 
+            this.flatMini1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatMini1.BackColor = System.Drawing.Color.White;
+            this.flatMini1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.flatMini1.Font = new System.Drawing.Font("Marlett", 12F);
+            this.flatMini1.Location = new System.Drawing.Point(549, 12);
+            this.flatMini1.Name = "flatMini1";
+            this.flatMini1.Size = new System.Drawing.Size(18, 18);
+            this.flatMini1.TabIndex = 11;
+            this.flatMini1.Text = "flatMini1";
+            this.flatMini1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // flatClose1
+            // 
+            this.flatClose1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flatClose1.BackColor = System.Drawing.Color.White;
+            this.flatClose1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.flatClose1.Font = new System.Drawing.Font("Marlett", 10F);
+            this.flatClose1.Location = new System.Drawing.Point(582, 12);
+            this.flatClose1.Name = "flatClose1";
+            this.flatClose1.Size = new System.Drawing.Size(18, 18);
+            this.flatClose1.TabIndex = 10;
+            this.flatClose1.Text = "flatClose1";
+            this.flatClose1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatClose1.Click += new System.EventHandler(this.flatClose1_Click);
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(171, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.fastColoredTextBox1.CaretColor = System.Drawing.Color.LightSalmon;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.CurrentLineColor = System.Drawing.Color.DimGray;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.White;
+            this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.SpringGreen;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(0, 49);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.PaddingBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.ServiceLinesColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(478, 259);
+            this.fastColoredTextBox1.TabIndex = 1;
+            this.fastColoredTextBox1.Text = "Made By Beast Gang";
+            this.fastColoredTextBox1.Zoom = 100;
+            this.fastColoredTextBox1.Load += new System.EventHandler(this.fastColoredTextBox1_Load);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.flatButton8);
+            this.panel1.Controls.Add(this.flatButton3);
+            this.panel1.Controls.Add(this.flatButton6);
+            this.panel1.Controls.Add(this.flatButton7);
+            this.panel1.Controls.Add(this.flatButton5);
+            this.panel1.Controls.Add(this.flatButton4);
+            this.panel1.Controls.Add(this.flatButton2);
+            this.panel1.Controls.Add(this.flatButton1);
+            this.panel1.Location = new System.Drawing.Point(479, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 259);
+            this.panel1.TabIndex = 7;
+            // 
+            // flatButton3
+            // 
+            this.flatButton3.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton3.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton3.Location = new System.Drawing.Point(5, 201);
+            this.flatButton3.Name = "flatButton3";
+            this.flatButton3.Rounded = false;
+            this.flatButton3.Size = new System.Drawing.Size(125, 26);
+            this.flatButton3.TabIndex = 20;
+            this.flatButton3.Text = "Update Api";
+            this.flatButton3.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton3.UseCustomColor = false;
+            this.flatButton3.Click += new System.EventHandler(this.flatButton3_Click);
+            // 
+            // flatButton6
+            // 
+            this.flatButton6.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton6.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton6.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton6.Location = new System.Drawing.Point(5, 133);
+            this.flatButton6.Name = "flatButton6";
+            this.flatButton6.Rounded = false;
+            this.flatButton6.Size = new System.Drawing.Size(125, 32);
+            this.flatButton6.TabIndex = 15;
+            this.flatButton6.Text = "Save File";
+            this.flatButton6.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton6.UseCustomColor = false;
+            this.flatButton6.Click += new System.EventHandler(this.flatButton6_Click);
+            // 
+            // flatButton7
+            // 
+            this.flatButton7.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton7.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton7.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton7.Location = new System.Drawing.Point(5, 229);
+            this.flatButton7.Name = "flatButton7";
+            this.flatButton7.Rounded = false;
+            this.flatButton7.Size = new System.Drawing.Size(125, 26);
+            this.flatButton7.TabIndex = 21;
+            this.flatButton7.Text = "Kill Roblox";
+            this.flatButton7.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton7.UseCustomColor = false;
+            this.flatButton7.Click += new System.EventHandler(this.flatButton7_Click);
+            // 
+            // flatButton5
+            // 
+            this.flatButton5.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton5.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton5.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton5.Location = new System.Drawing.Point(5, 37);
+            this.flatButton5.Name = "flatButton5";
+            this.flatButton5.Rounded = false;
+            this.flatButton5.Size = new System.Drawing.Size(125, 26);
+            this.flatButton5.TabIndex = 14;
+            this.flatButton5.Text = "Execute";
+            this.flatButton5.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton5.UseCustomColor = false;
+            this.flatButton5.Click += new System.EventHandler(this.flatButton5_Click);
+            // 
+            // flatButton4
+            // 
+            this.flatButton4.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton4.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton4.Location = new System.Drawing.Point(5, 99);
+            this.flatButton4.Name = "flatButton4";
+            this.flatButton4.Rounded = false;
+            this.flatButton4.Size = new System.Drawing.Size(125, 32);
+            this.flatButton4.TabIndex = 13;
+            this.flatButton4.Text = "Open File";
+            this.flatButton4.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton4.UseCustomColor = false;
+            this.flatButton4.Click += new System.EventHandler(this.flatButton4_Click_1);
+            // 
+            // flatButton2
+            // 
+            this.flatButton2.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton2.Location = new System.Drawing.Point(5, 65);
+            this.flatButton2.Name = "flatButton2";
+            this.flatButton2.Rounded = false;
+            this.flatButton2.Size = new System.Drawing.Size(125, 32);
+            this.flatButton2.TabIndex = 11;
+            this.flatButton2.Text = "Clear";
+            this.flatButton2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton2.UseCustomColor = false;
+            this.flatButton2.Click += new System.EventHandler(this.flatButton2_Click);
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton1.Location = new System.Drawing.Point(5, 167);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Rounded = false;
+            this.flatButton1.Size = new System.Drawing.Size(125, 32);
+            this.flatButton1.TabIndex = 0;
+            this.flatButton1.Text = "Settings";
+            this.flatButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton1.UseCustomColor = false;
+            this.flatButton1.Click += new System.EventHandler(this.flatButton1_Click);
+            // 
+            // flatButton8
+            // 
+            this.flatButton8.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton8.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton8.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.flatButton8.Location = new System.Drawing.Point(5, 3);
+            this.flatButton8.Name = "flatButton8";
+            this.flatButton8.Rounded = false;
+            this.flatButton8.Size = new System.Drawing.Size(125, 32);
+            this.flatButton8.TabIndex = 22;
+            this.flatButton8.Text = "Attach";
+            this.flatButton8.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.flatButton8.UseCustomColor = false;
+            this.flatButton8.Click += new System.EventHandler(this.flatButton8_Click);
+            // 
+            // BeastX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 308);
+            this.Controls.Add(this.formSkin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "BeastX";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form6";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.formSkin1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatUI.FormSkin formSkin1;
+        private System.Windows.Forms.Panel panel1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private FlatUI.FlatButton flatButton1;
+        private FlatUI.FlatButton flatButton6;
+        private FlatUI.FlatButton flatButton5;
+        private FlatUI.FlatButton flatButton4;
+        private FlatUI.FlatButton flatButton2;
+        private FlatUI.FlatButton flatButton3;
+        private FlatUI.FlatButton flatButton7;
+        private FlatUI.FlatMini flatMini1;
+        private FlatUI.FlatClose flatClose1;
+        private FlatUI.FlatButton flatButton8;
+    }
+}
